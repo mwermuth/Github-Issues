4845b38d8484c1904f36a88cce99cd90ad11362f
Elliot Larson <elliot@onehouse.net>
2012-01-06 17:47:38 -0800

    adding in issue class

---
+++ app/models/issue.rb
@@ -0,0 +1,22 @@
+class Issue
+  include ActiveModel::Validations
+  
+  attr_accessor :title, :message
+
+  validates :title, :presence => true
+  validates :message, :presence => true
+
+  def initialize(params = {})
+    params.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+
+  def to_key
+    nil
+  end
+  
+  def to_model
+    self
+  end
+end
