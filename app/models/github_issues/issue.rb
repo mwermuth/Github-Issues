4b4950c54703f287ce1748dda641f8705230e0f4
Elliot Larson <elliot@onehouse.net>
2012-01-08 17:45:54 -0800

    renaming github issue model and adding namespace

---
+++ app/models/github_issues/issue.rb
@@ -0,0 +1,24 @@
+module GithubIssues
+  class Issue
+    include ActiveModel::Validations
+    
+    attr_accessor :title, :message
+
+    validates :title, :presence => true
+    validates :message, :presence => true
+
+    def initialize(params = {})
+      params.each do |key, value|
+        send("#{key}=", value)
+      end
+    end
+
+    def to_key
+      nil
+    end
+    
+    def to_model
+      self
+    end
+  end
+end
