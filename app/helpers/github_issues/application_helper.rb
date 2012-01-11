460e595f24408609b6a4473377bc998d465da1f3
Elliot Larson <elliot@onehouse.net>
2012-01-10 19:18:41 -0800

    updating the helper and form templates to work with prepend and append title and body fields

--- app/helpers/github_issues/application_helper.rb
+++ app/helpers/github_issues/application_helper.rb
@@ -1,7 +1,10 @@
 module GithubIssues
   module ApplicationHelper
-    def github_issues
+    def github_issues(attributes = {})
       @issue = Issue.new
+      %w{prepend_title append_title prepend_body append_body}.each do |field|
+        @issue.send("#{field}=", attributes[:"#{field}"]) if attributes[:"#{field}"].present?
+      end
       render "github_issues/issues/new"
     end
   end
--- changelog.md
+++ changelog.md
@@ -1,3 +1,9 @@
+## v0.0.3
+
+* Added in prepend and append for body and title fields
+* Updating helper to accept values for append and prepend body and text
+  fields
+
 ## v0.0.2
 
 * Added in ActiveRecord callbacks for save method
